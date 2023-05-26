9370a79e6276b249eeded8260fd3904fb61beb97
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-05-26 09:39:08 +0200

    Layout improvement + detail fragment created

--- app/src/main/java/com/kgkk/marvelcomicsapp/ui/HomeFragment.kt
+++ app/src/main/java/com/kgkk/marvelcomicsapp/ui/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.kgkk.marvelcomicsapp.ui.home
+package com.kgkk.marvelcomicsapp.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/java/com/kgkk/marvelcomicsapp/ui/SearchFragment.kt
+++ app/src/main/java/com/kgkk/marvelcomicsapp/ui/SearchFragment.kt
@@ -1,7 +1,6 @@
-package com.kgkk.marvelcomicsapp.ui.search
+package com.kgkk.marvelcomicsapp.ui
 
 import android.content.Context
-import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
