63460f8730e227137cb0da05918e128729cf9783
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-05-26 12:26:25 +0200

    Back icon + layout improvements

--- app/src/main/java/com/kgkk/marvelcomicsapp/ui/DetailsFragment.kt
+++ app/src/main/java/com/kgkk/marvelcomicsapp/ui/DetailsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.kgkk.marvelcomicsapp.R
 import com.kgkk.marvelcomicsapp.databinding.FragmentDetailsBinding
 
@@ -29,7 +31,19 @@ class DetailsFragment : Fragment() {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.iconTextView.text = screen + selectedComic.toString()
+        // Set up the toolbar
+        val toolbar = binding.toolbar
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+        (requireActivity() as AppCompatActivity).supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true) // Show the back button
+            setDisplayShowHomeEnabled(true) // Show the back button as an icon
+            title = getString(R.string.details_title) // Set the title
+            setDisplayShowTitleEnabled(true) // Show the title
+        }
+        toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp() // Handle back button click
+        }
+
 
         return root
     }
